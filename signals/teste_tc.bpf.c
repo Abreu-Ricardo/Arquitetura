822db5de52e64fc97623aa005036cf334bc0a9fc
Abreu-Ricardo <61216670+Abreu-Ricardo@users.noreply.github.com>
2025-04-07 00:23:52 -0400

    teste_tc.bpf.c usando a minha_func()
    Agora consulta o mapa e verifica o pid para enviar o sinal para ele.

--- signals/teste_tc.bpf.c
+++ signals/teste_tc.bpf.c
@@ -1,4 +1,5 @@
 // signal_monitor.bpf.c
+
 #include <linux/types.h>
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
@@ -14,22 +15,41 @@
 //#include "vmlinux.h"
 
 //struct {
-// __uint(type, BPF_MAP_TYPE_PROG_ARRAY);
-// __uint(max_entries, 1);
-// __uint(key, __u32);
-// __uint(value, sizeof(__u32));
+//	__uint(type, BPF_MAP_TYPE_ARRAY);
+//	__uint(max_entries, 1);
+//	__type(key, __u32);
+//	__type(value, sizeof(pid_t));
 //} valores SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, __u32);
+    __type(value, sizeof(pid_t));
+    __uint(pinning, LIBBPF_PIN_BY_NAME);
+} valores SEC(".maps");
 
 
 
 SEC("tc")
 int pega_pkt(struct __sk_buff *ctx) {
 
-    __u32 *key = 0;
+    __u32 key = 0;
+    __u64 *ptr = NULL;
+    
+    ptr =  bpf_map_lookup_elem( &valores, &key);
+    if  (ptr == NULL ){
+    	return TC_ACT_OK;
+    }
+    //
+    __u32 temp = bpf_minha_func(*ptr);
 
-    //bpf_map_lookup_elem( &valores, key);
+    if (temp < 0){
+	    bpf_printk("Erro ao enviar o sinal\n");
+    	return TC_ACT_OK;
+    }
 
-    bpf_printk("ESTA CAPTANDO PKTS!!!\n");
+    bpf_printk("ESTA CAPTANDO PKTS!!! valor do pid, mapa--> %d\n",  *ptr);
 
     return TC_ACT_OK;
 }
